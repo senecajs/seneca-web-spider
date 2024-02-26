a3a76dea79eae881ae9caaaee9f16e63c37d195e
Jose Perigolo <neto.perigolo@gmail.com>
2024-02-26 12:21:30 -0300

    Create web-spider-doc.ts

---
+++ src/web-spider-doc.ts
@@ -0,0 +1,15 @@
+/* Copyright © 2022 Seneca Project Contributors, MIT License. */
+
+const docs = {
+  messages: {
+    get_info: {
+      desc: 'WEB Crawler SDK for seneca.',
+    },
+  },
+}
+
+export default docs
+
+if ('undefined' !== typeof (module)) {
+  module.exports = docs
+}
